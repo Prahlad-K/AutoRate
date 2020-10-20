5ef932bef01e48ad619f9910c09345f524f346a7
Madhur Vishwakarma <madhurbhopalsharma@gmail.com>
2020-10-20 16:37:43 +0530

    Included the Location Feature

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_app/app_screens/my_searchable_dropdown.dart';
-//import 'package:flutter_app/app_screens/test_search_bar.dart';
-
-//import 'my_button_widget.dart';
-import 'my_search_widget.dart';
+import 'my_button_widget.dart';
+import 'my_location_widget.dart';
 import 'my_stateful_widget.dart';
-
-//final _minimumPadding = 5.0;
 
 /// This is the main application widget.
 class MyApp extends StatelessWidget {
@@ -18,22 +13,23 @@ class MyApp extends StatelessWidget {
       title: _title,
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
-
         body: Center(
           child: Container(
             width: 400,
             height: 400,
             child: Column(
-              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 Text('Select Category:'),
                 MyStatefulWidget(),
-                Expanded(
-                  // child: MySearchWidget(),
-                  child: MySearchWidget(),
-                ),
-//                MyButtonWidget(),
-              ],
+                // Expanded(
+                //child: MySearchWidget(),
+                // ),
+               Expanded(
+                 child: DashboardScreen(),
+
+               ),
+                MyButtonWidget(),
+               ],
             ),
           ),
         )
